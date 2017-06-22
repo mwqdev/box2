0913209815c35015fa755f38a363d920e881adcf
mwqdev <quarles.michael@gmail.com>
2017-06-22 19:31:02 -0400

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET users listing. */
+// router.get('/', function (req, res, next) {
+//     res.send('respond with a resource');
+// });
+//
+// router.get('/detail', (req, res, next) => {
+//     res.send('details');
+// });
+//
+// module.exports = router;
+
+//TODO for later
